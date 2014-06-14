8d24d1b97308742de64b9b2aa901b2209360ac82
LiJunjie <holysoros@gmail.com>
2014-06-14 21:53:12 +0800

    增加经销地列表

--- paibei/models.py
+++ paibei/models.py
@@ -11,16 +11,18 @@ class User(Document):
     time = DateTimeField(default=datetime.datetime.now)
 
 
-places = (('TW', u'台湾'),
-          ('SH', u'上海'),
-          ('US', u'美国'))
-
-cities = (u'上海', u'北京', u'天津')
+places = (u'台湾', u'上海', u'美国')
+cities = (u'上海', u'北京', u'天津', u'重庆', u'安徽', u'福建',
+          u'甘肃', u'广东', u'贵州', u'河北', u'黑龙江', u'河南',
+          u'湖北', u'湖南', u'吉林', u'江西', u'江苏', u'辽宁',
+          u'山东', u'陕西', u'山西', u'四川', u'云南', u'浙江',
+          u'青海', u'海南', u'台湾', u'广西', u'内蒙古', u'宁夏',
+          u'西藏', u'新疆')
 
 
 class Product(Document):
     name = StringField(required=True, unique=True)
-    place = StringField(choices=places, default='TW', required=True)
+    place = StringField(choices=places, default=u'台湾', required=True)
     elements = StringField()
     image = ImageField(size=(1024, 768, True), thumbnail_size=(200, 200, True))
 
